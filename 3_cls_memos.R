eb281e7e1b15358dfc8662f1f7a69bba0e77f246
Sara Brumfield <sarabrumfield@gmail.com>
2022-11-30 09:43:52 -0500

    improved quasi agencies

--- 3_cls_memos.R
+++ 3_cls_memos.R
@@ -10,6 +10,8 @@ library(scales)
 
 devtools::load_all("G:/Analyst Folders/Sara Brumfield/_packages/bbmR")
 
+source("G:/Budget Publications/automation/0_data_prep/bookHelpers/R/tables.R")
+
 analysts <- import("G:/Analyst Folders/Sara Brumfield/_ref/Analyst Assignments.xlsx") %>%
   filter(!is.na(Analyst)) %>%
   select(`Agency ID`:Analyst)
@@ -26,17 +28,30 @@ adjustments <- readxl::read_excel("G:/Fiscal Years/Fiscal 2024/Planning Year/1. 
   filter(!is.na(`Agency Name`)) %>%
   relocate(`$ Change vs FY23 Adopted`, .after = `FY24 CLS`)
 
-# positions <- readxl::read_excel("G:/Fiscal Years/Fiscal 2024/Planning Year/1. CLS/2. Position Reports/PositionsSalariesOpcs_2022-11-2b_CLS FINAL AFTER BPFS.xlsx",
-#                                 sheet = "FY24 CLS") %>%
-#   mutate(`Agency ID` = as.numeric(`AGENCY ID`)) %>%
-#   left_join(analysts, by = "Agency ID")  %>%
-#   mutate(`Program ID` = as.character(`PROGRAM ID`))
+agencies <- unique(adjustments$`Agency Name`)
+# remaining_agencies <- agencies[length(agencies) - (1:25)]
 
-# pm <- import("C:/Users/sara.brumfield2/OneDrive - City Of Baltimore/Scorecard PMs_Recommendations_BBMR.xlsx", which = "Scorecard MAP") %>%
-#   select(`Agency Name`, `Service.ID`, `Program Name`, `Scorecard Measure`, `BBMR Decision`) %>%
-#   filter(grepl("NO|MAYBE", `BBMR Decision`)) %>%
-#   rename("Service ID" = "Service.ID")
+map(agencies, function(x) {
+  
+  knitr::knit_meta(class = NULL, clean = TRUE)
+  agency_clean <- analysts$`Agency Name - Cleaned`[analysts$`Agency Name` == x]
+  agency <- analysts$`Agency Name`[analysts$`Agency Name` == x]
+  type <- "agency"
+  
+  
+  rmarkdown::render(
+    'r/cls_memo_main.qmd',
+    output_file = paste0(
+      "FY24 CLS ", 
+      agency_clean, 
+      ".pdf"),
+    output_dir = "outputs")
+} 
+)
 
+
+
+##quasis ========================
 #assign quasi-agencies
 adjustments$`Agency Name`[adjustments$`Program ID` == "493" & adjustments$`Activity ID` == 1] <- "Baltimore Symphony Orchestra"
 adjustments$`Program ID`[adjustments$`Program ID` == "493" & adjustments$`Activity ID` == 1] <- "493c"
@@ -57,63 +72,13 @@ adjustments$`Program ID`[adjustments$`Program ID` == "590" & adjustments$`Activi
 adjustments$`Agency Name`[adjustments$`Program ID` == "590" & adjustments$`Activity ID` == 44] <- "Baltimore Public Markets"
 adjustments$`Program ID`[adjustments$`Program ID` == "590" & adjustments$`Activity ID` == 44] <- "590a"
 
-agencies <- unique(adjustments$`Agency Name`)
-remaining_agencies <- agencies[length(agencies) - (1:25)]
-problem_agencies <- c("M-R: Miscellaneous General Expenses", "M-R: Office of Neighborhood Safety and Engagement",              
-                      "Legal Aid",                                         "Family League",                                    
-                      "Baltimore Heritage Area",                           "Lexington Market",                                 
-                      "Baltimore Public Markets",                          "Visit Baltimore",                                  
-                      "Baltimore Symphony Orchestra",                      "Walters Art Museum",                               
-                      "Baltimore Museum of Art",                           "Maryland Zoo",                                    
-                      "Baltimore Office of Promotion & the Arts")
+quasi_agencies <- c("Legal Aid",                                         "Family League",                                    
+                    "Baltimore Heritage Area",                           "Lexington Market",                                 
+                    "Baltimore Public Markets",                          "Visit Baltimore",                                  
+                    "Baltimore Symphony Orchestra",                      "Walters Art Museum",                               
+                    "Baltimore Museum of Art",                           "Maryland Zoo",                                    
+                    "Baltimore Office of Promotion & the Arts")
 
-
-map(problem_agencies, function(x) {
-  
-  knitr::knit_meta(class = NULL, clean = TRUE)
-  agency_clean <- analysts$`Agency Name - Cleaned`[analysts$`Agency Name` == x]
-  agency <- analysts$`Agency Name`[analysts$`Agency Name` == x]
-  type <- "agency"
-  
-  
-  rmarkdown::render(
-    'r/cls_memo_main.qmd',
-    output_file = paste0(
-      "FY24 CLS ", 
-      agency_clean, 
-      ".pdf"),
-    output_dir = "outputs/_FY24 CLS")
-} 
-)
-
-# problem agencies ================
-##build in a try-catch
-adjustments %>%
-  filter(`Agency Name` == "Family League") %>%
-  rename(Service = `Program Name`) %>%
-  # mutate(`Service - Activity` = paste0(`Program Name`, "-" ,`Activity Name`)) %>%
-  select(Service, `Fund Name`, `FY23 Adopted`, `FY24 CLS`) %>%
-  group_by(`Fund Name`, Service) %>%
-  summarise(`FY23 Adopted` = sum(`FY23 Adopted`, na.rm = TRUE),
-            `FY24 CLS` = sum(`FY24 CLS`, na.rm = TRUE)) %>%
-  mutate_if(is.numeric, scales::dollar, style_negative = "parens") %>%
-  mutate(`FY24 CLS` = case_when(is.na(`FY24 CLS`) ~ replace_na("--"),
-                                TRUE ~ `FY24 CLS`))
-
-knitr::knit_meta(class = NULL, clean = TRUE)
-agency_clean <- analysts$`Agency Name - Cleaned`[analysts$`Agency Name` == x]
-agency <- analysts$`Agency Name`[analysts$`Agency Name` == x]
-type <- "agency"
-
-rmarkdown::render(
-  'r/cls_memo_single_line.qmd',
-  output_file = paste0(
-    "FY24 CLS ", 
-    agency_clean, 
-    ".pdf"),
-  output_dir = "outputs/_FY24 CLS")
-
-##quasis ========================
 quasis <- c(                     
   # "Legal Aid",                                         "Family League",
                                   "Baltimore Heritage Area",                           "Lexington Market",                                 
